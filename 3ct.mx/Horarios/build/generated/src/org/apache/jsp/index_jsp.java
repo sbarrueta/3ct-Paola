d70b96f6c302f5bc575a30c5b2dbfeb21c3d7fe0
sanchez, jorge <sbarrueta@gmail.com>
2019-02-22 15:09:11 -0600

    Se agrego la primera version de Paola, index.html con bootstrap

--- 3ct.mx/Horarios/build/generated/src/org/apache/jsp/index_jsp.java
+++ 3ct.mx/Horarios/build/generated/src/org/apache/jsp/index_jsp.java
@@ -1,132 +1,132 @@
-package org.apache.jsp;
-
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.jsp.*;
-import com.tresct.horarios.conexion.Conector;
-import com.tresct.horarios.conexion.ConsultaSQL;
-import org.json.simple.JSONObject;
-
-public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
-    implements org.apache.jasper.runtime.JspSourceDependent {
-
-  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
-
-  private static java.util.Vector _jspx_dependants;
-
-  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
-
-  public Object getDependants() {
-    return _jspx_dependants;
-  }
-
-  public void _jspService(HttpServletRequest request, HttpServletResponse response)
-        throws java.io.IOException, ServletException {
-
-    PageContext pageContext = null;
-    HttpSession session = null;
-    ServletContext application = null;
-    ServletConfig config = null;
-    JspWriter out = null;
-    Object page = this;
-    JspWriter _jspx_out = null;
-    PageContext _jspx_page_context = null;
-
-    try {
-      response.setContentType("text/html;charset=UTF-8");
-      pageContext = _jspxFactory.getPageContext(this, request, response,
-      			null, true, 8192, true);
-      _jspx_page_context = pageContext;
-      application = pageContext.getServletContext();
-      config = pageContext.getServletConfig();
-      session = pageContext.getSession();
-      out = pageContext.getOut();
-      _jspx_out = out;
-      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
-
-      out.write("\n");
-      out.write("\n");
-      out.write("\n");
-      out.write("\n");
-      out.write("\n");
-      out.write("<script type=\"text/javascript\" src=\"js/jquery.js\"></script>\n");
-      out.write("<script type=\"text/javascript\" src=\"js/jquery-ui.js\"></script>\n");
-      out.write("<link type=\"text/css\" rel=\"stylesheet\" href=\"css/jquery-ui.css\"/>\n");
-      out.write("<style>\n");
-      out.write("    .barra{\n");
-      out.write("        background-color: transparent;\n");
-      out.write("        display: inline-block;\n");
-      out.write("        width: 90px;\n");
-      out.write("        height: 15px;\n");
-      out.write("        float: right;\n");
-      out.write("        -moz-border-radius-topleft: 5px;\n");
-      out.write("        -moz-border-radius-topright:5px;\n");
-      out.write("        -moz-border-radius-bottomleft:5px;\n");
-      out.write("        -moz-border-radius-bottomright:5px;\n");
-      out.write("        -webkit-border-top-left-radius:5px;\n");
-      out.write("        -webkit-border-top-right-radius:5px;\n");
-      out.write("        -webkit-border-bottom-left-radius:5px;\n");
-      out.write("        -webkit-border-bottom-right-radius:5px;\n");
-      out.write("        border-top-left-radius:5px;\n");
-      out.write("        border-top-right-radius:5px;\n");
-      out.write("        border-bottom-left-radius:5px;\n");
-      out.write("        border-bottom-right-radius:5px;\n");
-      out.write("        /* Hack for IE 7 */\n");
-      out.write("        zoom: 1;\n");
-      out.write("        \n");
-      out.write("    }\n");
-      out.write("    .fechaInicio{\n");
-      out.write("        background-color: BLACK;        \n");
-      out.write("        color: RED;        \n");
-      out.write("        font-weight: bold;        \n");
-      out.write("        padding: 3px;        \n");
-      out.write("        border-radius: 3px;        \n");
-      out.write("        -webkit-border-radius: 3px;        \n");
-      out.write("        -moz-border-radius: 3px;    \n");
-      out.write("    }    \n");
-      out.write("    p{        \n");
-      out.write("        color: gray;        \n");
-      out.write("        line-height: 20px;        \n");
-      out.write("        padding: 5px 0 0 10px;\n");
-      out.write("    }\n");
-      out.write("    .p{\n");
-      out.write("        display: inline-block;\n");
-      out.write("        padding-left: 10px;\n");
-      out.write("        color: RED;\n");
-      out.write("        margin-bottom: 10px;\n");
-      out.write("    }\n");
-      out.write("    .t{\n");
-      out.write("        padding-left: 10px;\n");
-      out.write("    }\n");
-      out.write("    #tit{        \n");
-      out.write("        color: #020f49;    \n");
-      out.write("    }\n");
-      out.write("    /*.lleno{\n");
-      out.write("        background-image: url(imgs/152.gif);\n");
-      out.write("    }\n");
-      out.write("    .medio{\n");
-      out.write("        background-image: url(imgs/152.gif);\n");
-      out.write("    }\n");
-      out.write("    .libre{\n");
-      out.write("        background-image: url(imgs/152.gif);\n");
-      out.write("    }*/\n");
-      out.write("    .carga{\n");
-      out.write("        /*background-image: url(imgs/barraOk.gif);*/\n");
-      out.write("        background-color: #bcf209;\n");
-      out.write("        -moz-border-radius-topleft:4px;\n");
-      out.write("        -moz-border-radius-topright:4px;\n");
-      out.write("        -moz-border-radius-bottomleft:4px;\n");
-      out.write("        -moz-border-radius-bottomright:4px;\n");
-      out.write("        -webkit-border-top-left-radius:4px;\n");
-      out.write("        -webkit-border-top-right-radius:4px;\n");
-      out.write("        -webkit-border-bottom-left-radius:4px;\n");
-      out.write("        -webkit-border-bottom-right-radius:4px;\n");
-      out.write("        border-top-left-radius:4px;\n");
-      out.write("        border-top-right-radius:4px;\n");
-      out.write("        border-bottom-left-radius:4px;\n");
-      out.write("        border-bottom-right-radius:4px;\n");
-      out.write("    }\n");
-      out.write("</style>\n");
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+import com.tresct.horarios.conexion.Conector;
+import com.tresct.horarios.conexion.ConsultaSQL;
+import org.json.simple.JSONObject;
+
+public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.Vector _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public Object getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("<script type=\"text/javascript\" src=\"js/jquery.js\"></script>\n");
+      out.write("<script type=\"text/javascript\" src=\"js/jquery-ui.js\"></script>\n");
+      out.write("<link type=\"text/css\" rel=\"stylesheet\" href=\"css/jquery-ui.css\"/>\n");
+      out.write("<style>\n");
+      out.write("    .barra{\n");
+      out.write("        background-color: transparent;\n");
+      out.write("        display: inline-block;\n");
+      out.write("        width: 90px;\n");
+      out.write("        height: 15px;\n");
+      out.write("        float: right;\n");
+      out.write("        -moz-border-radius-topleft: 5px;\n");
+      out.write("        -moz-border-radius-topright:5px;\n");
+      out.write("        -moz-border-radius-bottomleft:5px;\n");
+      out.write("        -moz-border-radius-bottomright:5px;\n");
+      out.write("        -webkit-border-top-left-radius:5px;\n");
+      out.write("        -webkit-border-top-right-radius:5px;\n");
+      out.write("        -webkit-border-bottom-left-radius:5px;\n");
+      out.write("        -webkit-border-bottom-right-radius:5px;\n");
+      out.write("        border-top-left-radius:5px;\n");
+      out.write("        border-top-right-radius:5px;\n");
+      out.write("        border-bottom-left-radius:5px;\n");
+      out.write("        border-bottom-right-radius:5px;\n");
+      out.write("        /* Hack for IE 7 */\n");
+      out.write("        zoom: 1;\n");
+      out.write("        \n");
+      out.write("    }\n");
+      out.write("    .fechaInicio{\n");
+      out.write("        background-color: BLACK;        \n");
+      out.write("        color: RED;        \n");
+      out.write("        font-weight: bold;        \n");
+      out.write("        padding: 3px;        \n");
+      out.write("        border-radius: 3px;        \n");
+      out.write("        -webkit-border-radius: 3px;        \n");
+      out.write("        -moz-border-radius: 3px;    \n");
+      out.write("    }    \n");
+      out.write("    p{        \n");
+      out.write("        color: gray;        \n");
+      out.write("        line-height: 20px;        \n");
+      out.write("        padding: 5px 0 0 10px;\n");
+      out.write("    }\n");
+      out.write("    .p{\n");
+      out.write("        display: inline-block;\n");
+      out.write("        padding-left: 10px;\n");
+      out.write("        color: RED;\n");
+      out.write("        margin-bottom: 10px;\n");
+      out.write("    }\n");
+      out.write("    .t{\n");
+      out.write("        padding-left: 10px;\n");
+      out.write("    }\n");
+      out.write("    #tit{        \n");
+      out.write("        color: #020f49;    \n");
+      out.write("    }\n");
+      out.write("    /*.lleno{\n");
+      out.write("        background-image: url(imgs/152.gif);\n");
+      out.write("    }\n");
+      out.write("    .medio{\n");
+      out.write("        background-image: url(imgs/152.gif);\n");
+      out.write("    }\n");
+      out.write("    .libre{\n");
+      out.write("        background-image: url(imgs/152.gif);\n");
+      out.write("    }*/\n");
+      out.write("    .carga{\n");
+      out.write("        /*background-image: url(imgs/barraOk.gif);*/\n");
+      out.write("        background-color: #bcf209;\n");
+      out.write("        -moz-border-radius-topleft:4px;\n");
+      out.write("        -moz-border-radius-topright:4px;\n");
+      out.write("        -moz-border-radius-bottomleft:4px;\n");
+      out.write("        -moz-border-radius-bottomright:4px;\n");
+      out.write("        -webkit-border-top-left-radius:4px;\n");
+      out.write("        -webkit-border-top-right-radius:4px;\n");
+      out.write("        -webkit-border-bottom-left-radius:4px;\n");
+      out.write("        -webkit-border-bottom-right-radius:4px;\n");
+      out.write("        border-top-left-radius:4px;\n");
+      out.write("        border-top-right-radius:4px;\n");
+      out.write("        border-bottom-left-radius:4px;\n");
+      out.write("        border-bottom-right-radius:4px;\n");
+      out.write("    }\n");
+      out.write("</style>\n");
 
     Conector con=new Conector("tresct.com","tresctco_root","pasale","tresctco_3ct");
 
@@ -188,84 +188,84 @@ public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
                 out.println("<p id='tit'>Fines de semana:</p>");
                 cuenta2++;
             }
-
-      out.write("\n");
-      out.write("<div class=\"t\">\n");
-      out.write("    \n");
-      out.write("    \n");
-      out.write("    \n");
-      out.write("    ESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS\n");
-      out.write("    <span class=\"fechaInicio\" >");
-      out.print( datos[i][2] );
-      out.write(" de ");
-      out.print( mes );
-      out.write("</span>\n");
-      out.write("    ");
+
+      out.write("\n");
+      out.write("<div class=\"t\">\n");
+      out.write("    \n");
+      out.write("    \n");
+      out.write("    \n");
+      out.write("    ESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS\n");
+      out.write("    <span class=\"fechaInicio\" >");
+      out.print( datos[i][2] );
+      out.write(" de ");
+      out.print( mes );
+      out.write("</span>\n");
+      out.write("    ");
 
         if(noAlumnos!=0){
-    
-      out.write("\n");
-      out.write("    <div id=\"");
-      out.print( datos[i][1] );
-      out.write("\" class=\"barra\"></div>\n");
-      out.write("    ");
+    
+      out.write("\n");
+      out.write("    <div id=\"");
+      out.print( datos[i][1] );
+      out.write("\" class=\"barra\"></div>\n");
+      out.write("    ");
 
        }
-    
-      out.write("\n");
-      out.write("     \n");
-      out.write("</div>\n");
-      out.write("    <div class=\"p\">\n");
-      out.write("        ");
-      out.print( datos[i][5] );
-      out.write(" en \n");
-      out.write("        <strong>");
-      out.print( plantel );
-      out.write("</strong>\n");
-      out.write("    </div>\n");
-      out.write("    \n");
-      out.write("    <script type=\"text/javascript\">\n");
-      out.write("        $( \"#");
-      out.print( datos[i][1] );
-      out.write("\" ).progressbar({\n");
-      out.write("            value: ");
-      out.print( valor );
-      out.write("\n");
-      out.write("        });\n");
-      out.write("        $( \"#");
-      out.print( datos[i][1] );
-      out.write(" div\").addClass(\"");
-      out.print( tipo );
-      out.write("\");\n");
-      out.write("        $( \"#");
-      out.print( datos[i][1] );
-      out.write(" div\").addClass(\"carga\");\n");
-      out.write("    </script>\n");
+    
+      out.write("\n");
+      out.write("     \n");
+      out.write("</div>\n");
+      out.write("    <div class=\"p\">\n");
+      out.write("        ");
+      out.print( datos[i][5] );
+      out.write(" en \n");
+      out.write("        <strong>");
+      out.print( plantel );
+      out.write("</strong>\n");
+      out.write("    </div>\n");
+      out.write("    \n");
+      out.write("    <script type=\"text/javascript\">\n");
+      out.write("        $( \"#");
+      out.print( datos[i][1] );
+      out.write("\" ).progressbar({\n");
+      out.write("            value: ");
+      out.print( valor );
+      out.write("\n");
+      out.write("        });\n");
+      out.write("        $( \"#");
+      out.print( datos[i][1] );
+      out.write(" div\").addClass(\"");
+      out.print( tipo );
+      out.write("\");\n");
+      out.write("        $( \"#");
+      out.print( datos[i][1] );
+      out.write(" div\").addClass(\"carga\");\n");
+      out.write("    </script>\n");
 
         }
     }catch(Exception ex){        
         System.out.println("Error: "+ex.getMessage());
-
-      out.write("\n");
-      out.write("<p>\n");
-      out.write("    <span class=\"fechaInicio\" >Sin inicios proximos</span>\n");
-      out.write("</p>\n");
+
+      out.write("\n");
+      out.write("<p>\n");
+      out.write("    <span class=\"fechaInicio\" >Sin inicios proximos</span>\n");
+      out.write("</p>\n");
 
     }finally{
         con.cierraConexion();
     }
     out.flush();
-
-    } catch (Throwable t) {
-      if (!(t instanceof SkipPageException)){
-        out = _jspx_out;
-        if (out != null && out.getBufferSize() != 0)
-          out.clearBuffer();
-        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
-        else throw new ServletException(t);
-      }
-    } finally {
-      _jspxFactory.releasePageContext(_jspx_page_context);
-    }
-  }
-}
+
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
